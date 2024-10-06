f99ec2eb13ae4cc394556e5d00b8ddd5d5468fe1
Micah Cowell <micahcowell@gmail.com>
2024-10-06 22:42:41 +0000

    env

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install go CLI tools
-#   NOTE:
-# It's important to install these in a Dev Container "postCreateCommand" script,
-# instead of inside the Dockerfile. If these were executed within the Dockerfile,
-# then they would be executed as root and mess up file permissions.
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 
+# Create dotenv
+cp -n .env.public .env
+
 # Execute latest migration
-migrate -path=./migrations -database=$DATABASE_URL up
+echo "apply all up database migrations?"
+make db/migrations/up
